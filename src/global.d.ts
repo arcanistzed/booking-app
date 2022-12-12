7092bd6047028fe99bdcf7303dade1e119280a28
Daniel Thorp <arcanistzed@gmail.com>
2022-12-12 04:07:35 -0500

    style: :fire: unused declarations
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-
-declare module "react-datetime-picker/dist/entry.nostyle" {
-    export default class DateTimePicker extends Component<
-        unknown,
-        unknown
-    > { }
-}
